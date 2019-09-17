0f0a60e5dd9a5c9b0e2418108d24422c055b7116
Web Dev <WebDev@Asifs-iMac.local>
2019-09-17 14:18:27 -0700

    carousel_update_1

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,7 +5,6 @@
         <h1>{{$title}}</h1>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam iure voluptas nulla accusamus veniam vitae unde debitis optio, porro, a possimus placeat dolorem rerum sint ab dolores doloribus labore necessitatibus.</p>
         <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
-        <hr>
         @include('inc.slideshow')
     </div>
 @endsection
